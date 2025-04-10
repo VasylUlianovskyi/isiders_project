d6195c5665312d5c2ca281449fe05b0b694b66d9
vasyl <vasia.kit434@gmail.com>
2025-04-10 17:12:13 +0300

    Create user table

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,17 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 import app from './app';
 import sequelize from './config/sequelize';
+import User from './models/User';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server listen on port: ${PORT}`);
-});
-
 sequelize
   .authenticate()
-  .then(() => {
+
+  .then(async () => {
     console.log('Database connected');
+    console.log('Syncing with models:');
+    console.log(' User model:', User === undefined ? 'NOT DEFINED' : 'OK');
+
+    await sequelize.sync({ force: true });
     app.listen(PORT, () => {
       console.log(` Server running on http://localhost:${PORT}`);
     });
