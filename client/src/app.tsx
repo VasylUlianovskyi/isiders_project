3dd5b361fe84be0c8f1983ac550041b3432d26d3
vasyl <vasia.kit434@gmail.com>
2025-04-10 12:07:57 +0300

    Add react-router0dom, className library

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'preact/hooks';
-
 import './app.css';
+
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Footer from './components/Footer/Footer';
