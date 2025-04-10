e35ae9333e910e0d57eaefce47384a182ee280fd
vasyl <vasia.kit434@gmail.com>
2025-04-10 14:02:05 +0300

    Fix package conflict

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Footer from './components/Footer/Footer';
 
-export function App () {
+export default function App () {
   return (
     <>
       <Header />
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { App } from './app';
+import App from './App';
+
 import './reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
