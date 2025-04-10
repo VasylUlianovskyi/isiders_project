cb7269898c973232f8e178414b298715289494fe
vasyl <vasia.kit434@gmail.com>
2025-04-10 11:13:59 +0300

    Added Header, Footer and main components

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'preact/hooks'
+import { useState } from 'preact/hooks';
 
-import './app.css'
+import './app.css';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import Footer from './components/Footer/Footer';
 
 export function App () {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>initial</h1>
+      <Header />
+      <Home />
+      <Footer />
     </>
-  )
+  );
 }
