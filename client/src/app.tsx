0a21785ad32be8493658019e2af61b5036933236
vasyl <vasia.kit434@gmail.com>
2025-04-10 10:52:09 +0300

    initial commit

---
+++ client/src/app.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'preact/hooks'
+
+import './app.css'
+
+export function App () {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>initial</h1>
+    </>
+  )
+}
